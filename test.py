8a2643cdac9b26f9c29717a14cf88882fa4eb5c2
Michael Sealand <michael.sealand@plantronics.com>
2017-05-25 17:15:50 -0700

    Get data from S3???

---
+++ test.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pyart
+import boto3
+import tempfile
+
+# Let's use Amazon S3
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('noaa-nexrad-level2')
+s3key = '2016/12/15/KMUX/KMUX20161215_104149_V06'
+
+# download to a local file, and read it
+localfile = tempfile.mktemp()
+bucket.download_file(s3key, localfile)
+radar = pyart.io.read_nexrad_archive(localfile)
+
+# mask out last 10 gates of each ray, this removes the "ring" around the radar.
+radar.fields['reflectivity']['data'][:, -10:] = np.ma.masked
+
+# exclude masked gates from the gridding
+gatefilter = pyart.filters.GateFilter(radar)
+gatefilter.exclude_transition()
+gatefilter.exclude_masked('reflectivity')
+
+# perform Cartesian mapping, limit to the reflectivity field.
+grid = pyart.map.grid_from_radars(
+    (radar,), gatefilters=(gatefilter, ),
+    grid_shape=(1, 241, 241),
+    grid_limits=((2000, 2000), (-123000.0, 123000.0), (-123000.0, 123000.0)),
+    fields=['reflectivity'])
+
+# create the plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.imshow(grid.fields['reflectivity']['data'][0], origin='lower')
+plt.show()
